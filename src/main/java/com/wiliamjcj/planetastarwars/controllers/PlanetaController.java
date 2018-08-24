3b9dcb1b0f4670924e6ac655f151f23b22c78d66
wiljcj <wiljc@gmail.com>
2018-08-24 12:07:32 -0300

    Adicionando endpoint para adicionar um novo planeta, buscando a quantidade de participações em filmes da Star Wars API (swapi).

--- src/main/java/com/wiliamjcj/planetastarwars/controllers/PlanetaController.java
+++ src/main/java/com/wiliamjcj/planetastarwars/controllers/PlanetaController.java
@@ -1,20 +1,61 @@
 package com.wiliamjcj.planetastarwars.controllers;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wiliamjcj.planetastarwars.dto.PlanetaDTO;
 import com.wiliamjcj.planetastarwars.services.PlanetaService;
+import com.wiliamjcj.planetastarwars.utils.APIResponse;
+
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(value = "${controller.planeta.mapping}")
 public class PlanetaController {
+
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Value("${controller.planeta.mapping}")
 	private String BASE_ENDPOINT;
 
 	@Autowired
 	PlanetaService planetaService;
-	
+
+	@ApiOperation(value = "Adiciona um novo planeta, com nome, clima e terreno, retornando no header a sua localização.")
+	@PostMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<APIResponse<String>> adicionarPlaneta(@Valid @RequestBody PlanetaDTO planeta,
+			BindingResult res) {
+		try {
+			APIResponse<String> apiResponse = new APIResponse<String>();
+
+			if (res.hasErrors()) {
+				res.getAllErrors().stream().forEach(err -> apiResponse.getErrors().add(err.getDefaultMessage()));
+				return ResponseEntity.badRequest().body(apiResponse);
+			}
+			planeta.setId(null);
+			planeta = planetaService.criarPlaneta(planeta);
+			URI location = new URI(BASE_ENDPOINT + "/" + planeta.getId());
+
+			return ResponseEntity.created(location).body(apiResponse);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			log.debug(e.getStackTrace().toString());
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+
 }
--- src/main/java/com/wiliamjcj/planetastarwars/services/PlanetaService.java
+++ src/main/java/com/wiliamjcj/planetastarwars/services/PlanetaService.java
@@ -1,18 +1,76 @@
 package com.wiliamjcj.planetastarwars.services;
 
+import java.util.Arrays;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.wiliamjcj.planetastarwars.dto.DTOMapper;
+import com.wiliamjcj.planetastarwars.dto.PlanetaDTO;
+import com.wiliamjcj.planetastarwars.entities.Planeta;
 import com.wiliamjcj.planetastarwars.repositories.PlanetaRepository;
 
 @Service
 public class PlanetaService {
+
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	DTOMapper mapper;
 
 	@Autowired
 	PlanetaRepository planetaRepository;
-	
+
+	@Value("${rest.default.user-agent}")
+	private String userAgent;
+
+	@Value("${rest.swapi.uri}")
+	private String swapiURI;
+
+	public PlanetaDTO criarPlaneta(PlanetaDTO dto) {
+		Planeta planeta = (Planeta) mapper.map(dto, Planeta.class);
+
+		Integer qtdAparicoesFilmes = buscarPlanetaSWAPI(planeta.getNome());
+		planeta.setQtdAparicoesFilmes(qtdAparicoesFilmes);
+
+		planeta = planetaRepository.save(planeta);
+
+		return (PlanetaDTO) mapper.map(planeta, PlanetaDTO.class);
+	}
+
+	private Integer buscarPlanetaSWAPI(String nome) {
+		try {
+			RestTemplate restTemplate = new RestTemplate();
+			HttpHeaders headers = new HttpHeaders();
+			headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON_UTF8));
+			headers.add("user-agent", userAgent);
+			HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+			ResponseEntity<String> response = restTemplate.exchange(swapiURI + nome, HttpMethod.GET, entity,
+					String.class);
+
+			JSONObject jo = new JSONObject(response.getBody());
+			Integer qtd = jo.getInt("count");
+			if (qtd > 1) {
+				return 0;
+			}
+			JSONObject planeta = (JSONObject) jo.getJSONArray("results").get(0);
+			JSONArray filmes = planeta.getJSONArray("films");
+			return filmes.length();
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			log.debug(e.getStackTrace().toString());
+			return 0;
+		}
+	}
 }
