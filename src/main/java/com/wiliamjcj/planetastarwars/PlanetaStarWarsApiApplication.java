5d0706d5bd3b399fd5888ab16d19b4ffcfd917c7
wiljcj <wiljc@gmail.com>
2018-08-24 10:19:09 -0300

    Criando projeto spring boot com suporte ao mongodb

---
+++ src/main/java/com/wiliamjcj/planetastarwars/PlanetaStarWarsApiApplication.java
@@ -0,0 +1,12 @@
+package com.wiliamjcj.planetastarwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetaStarWarsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetaStarWarsApiApplication.class, args);
+	}
+}
